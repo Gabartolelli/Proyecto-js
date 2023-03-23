e95739bc44c7ad0a464b71d2289e68b47ff253a0
germanbartolelli <german.a.bartolelli@hotmail.com>
2023-03-23 15:07:15 -0300

    agregado la validacion del numero

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ while (gracias){
 const nombreApellido = prompt ("Ingrese su nombre y apellido")
 const domicilio = prompt ("Ingrese su domicilio")
 const localidad = prompt ("Ingrese su localidad")
-const CP = parseInt ( prompt ("Ingrese su codigo postal"))
+let CP ;
+do{
+   CP = parseInt ( prompt ("Ingrese su codigo postal"))
+}while (isNaN (CP))
 const provincia = prompt ("Ingrese su provincia")
 const envioRegional = 1500.00
 const envioNacional = 2000.00
 
 if(CP<= 4000) {
-    alert("Gracias por su compra "+ nombreApellido+ " " + domicilio + " " + localidad +" "+ CP +" "+ provincia + " costo de envio "+ envioRegional)
+    alert("Gracias por su compra "+ nombreApellido+ " su costo de envio es de "+ envioRegional)
 }
 else if (CP > 4000){
 
-    alert("Gracias por su compra "+ nombreApellido+ " " + domicilio + " " + localidad +" "+ CP +" "+ provincia + " costo de envio "+ envioNacional)
-}
+    alert("Gracias por su compra "+ nombreApellido+ " su costo de envio es de "+ envioNacional)
 gracias = false
 }
-
+}
