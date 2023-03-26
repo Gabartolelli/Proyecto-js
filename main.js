a17cb31ab33ab85f5a6bf3c77bdbc223ce96fe60
germanbartolelli <german.a.bartolelli@hotmail.com>
2023-03-26 11:29:20 -0300

    no sale alert

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-
-let gracias = true
-while (gracias){
+const codigoPostal = (mensaje) => {
+    let CP
+    do{
+        CP = parseInt ( prompt (mensaje))
+     }while (isNaN (CP))
+}
+let costoEnvio = true
+while (costoEnvio){
 let nombreApellido = prompt ("Ingrese su nombre y apellido")
 let domicilio = prompt ("Ingrese su domicilio")
 let localidad = prompt ("Ingrese su localidad")
-let CP ;
-do{
-   CP = parseInt ( prompt ("Ingrese su codigo postal"))
-}while (isNaN (CP))
+let CP = codigoPostal ("ingrese su codigo postal")
 let provincia = prompt ("Ingrese su provincia")
 const envioRegional = 1500.00
 const envioNacional = 2000.00
 
-if(CP<= 4000) {
+if(CP <= 4000) {
     alert("Gracias por su compra "+ nombreApellido + " su costo de envio es de "+ envioRegional)
+    
 }
 
-else if (CP > 4000){
+else if (CP> 4000){
     alert("Gracias por su compra "+ nombreApellido + " su costo de envio es de "+ envioNacional)
 }
 break
+
 }
