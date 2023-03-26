1b05d55669a5c36e6345132a0067822acf97be08
germanbartolelli <german.a.bartolelli@hotmail.com>
2023-03-26 10:46:13 -0300

    inclui el break para salir del while

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
+
 let gracias = true
 while (gracias){
-const nombreApellido = prompt ("Ingrese su nombre y apellido")
-const domicilio = prompt ("Ingrese su domicilio")
-const localidad = prompt ("Ingrese su localidad")
+let nombreApellido = prompt ("Ingrese su nombre y apellido")
+let domicilio = prompt ("Ingrese su domicilio")
+let localidad = prompt ("Ingrese su localidad")
 let CP ;
 do{
    CP = parseInt ( prompt ("Ingrese su codigo postal"))
 }while (isNaN (CP))
-const provincia = prompt ("Ingrese su provincia")
+let provincia = prompt ("Ingrese su provincia")
 const envioRegional = 1500.00
 const envioNacional = 2000.00
 
 if(CP<= 4000) {
-    alert("Gracias por su compra "+ nombreApellido+ " su costo de envio es de "+ envioRegional)
+    alert("Gracias por su compra "+ nombreApellido + " su costo de envio es de "+ envioRegional)
 }
-else if (CP > 4000){
 
-    alert("Gracias por su compra "+ nombreApellido+ " su costo de envio es de "+ envioNacional)
-gracias = false
+else if (CP > 4000){
+    alert("Gracias por su compra "+ nombreApellido + " su costo de envio es de "+ envioNacional)
 }
+break
 }
