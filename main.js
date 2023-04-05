86c4ca51f360860a91e633b38c541b19413e37f0
germanbartolelli <german.a.bartolelli@hotmail.com>
2023-04-05 20:46:49 -0300

    comenzando la segunda pre entrega

--- main.js
+++ main.js
@@ -1,4 +1,77 @@
 
+const productos = []
+const ambo = () =>{
+    const talle = prompt( "Ingrese su talle: S/M/L/XL/XXL")
+    const color = prompt("Ingrese color (blanco/negro")
+    const precio = 2500
+    productos.push({
+        talle:talle,
+        color:color,
+        precio:precio
+    })
+}
+const guardapolvo = () =>{
+    const talle = prompt( "Ingrese su talle: S/M/L/XL/XXL")
+    const color = prompt("Ingrese color (rojo/azul")
+    const precio = 3500
+    productos.push({
+        talle:talle,
+        color:color,
+        precio:precio
+    })
+}
+const magisterio = () =>{
+    const talle = prompt( "Ingrese su talle: S/M/L/XL/XXL")
+    const color = prompt("Ingrese color")
+    const precio = 3000
+    productos.push({
+        talle:talle,
+        color:color,
+        precio:precio
+    })
+}
+const overol = () =>{
+    const talle = prompt( "Ingrese su talle: S/M/L/XL/XXL")
+    const color = prompt("Ingrese color (azul/gris")
+    const precio = 5000
+    productos.push({
+        talle:talle,
+        color:color,
+        precio:precio
+    })
+}
+
+
+
+
+    
+
+const verProductos = (producto) =>{
+    
+    while(producto){
+        const accion = prompt ("Seleccione el producto que desea comprar: 1:Ambo/2:Guardapolvo infantil/3:Magisterio/4:Overoles.")
+   
+    switch(accion){
+        case "1":
+            ambo()
+            break
+        case "2":
+            guardapolvo()
+            break
+        case "3":
+            magisterio()
+            break
+        case "4":
+            overol ()
+        default:
+            alert("Producto inexistente")
+    } 
+    producto = confirm ("¿desea seguir comprando?")
+}
+}
+
+verProductos (true)
+/*
 const envioRegional = 1500
 const envioNacional = 2000
 
@@ -29,3 +102,4 @@ while (continuar){
 continuar= confirm ("¿desea editar algun dato?")
 }
 alert("Su envio esta siendo procesado")
+*/
