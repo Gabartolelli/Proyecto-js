e19f801da3b3d46a5ba272effe8e0c39bd3ebb2d
germanbartolelli <german.a.bartolelli@hotmail.com>
2023-04-28 19:03:20 -0300

    casi listo

--- main.js
+++ main.js
@@ -1,3 +1,168 @@
+const tarjetas = document.getElementById("tarjetas")
+const verCarrito = document.getElementById("verCarrito")
+const contenedorProducto = document.getElementById("contenedorProducto")
+
+const productos=[
+    {
+        id:1,
+        nombre:" Guardapolvo",
+        precio:5000,
+        img:"./assets/img/guardapolvo.png"
+    },
+    {
+        id:2,
+        nombre:"Ambo",
+        precio:4500,
+        img:"./assets/img/ambo.jpg"
+
+
+    },
+    {
+        id:3,
+        nombre:"Gastronomia",
+        precio:6000,
+        img:"./assets/img/saco chef.jpg"
+
+
+    },
+    {
+        id:4,
+        nombre:"Magisterio",
+        precio:5500,
+        img:"./assets/img/ponchos-magist-cat.jpg"
+
+
+    }
+]
+
+let carrito = []
+
+productos.forEach((producto) => {
+    let tarjeta = document.createElement("div")
+    tarjeta.className="tarjeta"
+    tarjeta.innerHTML = `
+    <img src="${producto.img}">
+    <h3>${producto.nombre}</h3>
+    <p class="precio">$${producto.precio}</p>`
+
+    tarjetas.append(tarjeta)
+
+    let agregarCarrito = document.createElement("button")
+    agregarCarrito.innerText = "Agregar al carrito"
+    agregarCarrito.className = "agregar"
+    
+    tarjeta.append(agregarCarrito)
+
+    agregarCarrito.addEventListener("click",()=>{
+        carrito.push({
+            id:producto.id,
+            img:producto.img,
+            nombre:producto.nombre,
+            precio:producto.precio
+        })
+    })
+});
+
+verCarrito.addEventListener("click", ()=>{
+    contenedorProducto.innerHTML=""
+    contenedorProducto.style.display = "flex"
+    const headerCarrito = document.createElement("div")
+    headerCarrito.className = "headerCarrito"
+    headerCarrito.innerHTML = `
+        <h1 class="tituloHeader">Carrito</h1>
+        `
+
+    contenedorProducto.append(headerCarrito)
+
+    const btnCarrito = document.createElement("button")
+        btnCarrito.innerHTML = `
+        <button class="btnCarrito">X</button>
+        `
+    btnCarrito.addEventListener("click",()=>{
+        contenedorProducto.style.display="none"
+    })
+    contenedorProducto.append(btnCarrito)
+
+    carrito.forEach((producto)=>{
+        let contenedorCarrito = document.createElement("div")
+        contenedorCarrito.className = "contenedorCarrito"
+        contenedorCarrito.innerHTML = `
+        <img src="${producto.img}">
+        <h3>${producto.nombre}</h3>
+        <p>$${producto.precio}</p>
+        `
+    
+    contenedorProducto.append(contenedorCarrito)
+    
+})
+const total=carrito.reduce((acc,e) => acc + e.precio,0)
+
+    const totalCompra= document.createElement("div")
+    totalCompra.className ="totalCompra"
+    totalCompra.innerHTML =`Total a pagar: $${total}
+    `
+    contenedorProducto.append(totalCompra)
+})
+
+
+/*
+class Producto {
+    constructor(nombre,talle,precio){
+        this.nombre = nombre
+        this.talle = talle
+        this.precio = parseFloat(precio) 
+    }
+}
+
+const productos = JSON.parse(localStorage.getItem("productos")) ?? []
+
+const crearProducto = () =>{
+    const formularioCrear = document.querySelector("#crearProducto")
+    formularioCrear.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        const datos = e.target.children
+        const producto = new Producto(datos["nombre"].value,datos["talle"].value,datos["precio"].value)
+        productos.push(producto)
+        localStorage.setItem("productos",JSON.stringify(productos))
+        formularioCrear.reset()
+    })
+}
+
+
+
+const ContenedorProductos = document.querySelector("#productos")
+productos.forEach(producto => {
+    const tarjetaProducto = document.createElement("div")
+    tarjetaProducto.className = "producto"
+    tarjetaProducto.innerHTML = `
+        <img src="./assets/img/tv-with-wide-screen.jpg" alt="">
+        <h3>${producto.nombre}</h3>
+        <input type="text" name="talle" value="${producto.talle}">
+        <p> "$"${producto.precio}</p>
+        <button class="btn">Agregar al carrito</button>`
+ContenedorProductos.append(tarjetaProducto)
+})
+
+const verProductos = () =>{
+    const ContenedorProductos = document.querySelector("#productos")
+    productos.forEach(producto => {
+        const tarjetaProducto = document.createElement("div")
+        tarjetaProducto.className = "producto"
+        tarjetaProducto.innerHTML = `
+            <img src="./src/img/tv-with-wide-screen.jpg" alt="">
+            <form class="editar">
+                <input type="text" name="nombre" value="${producto.nombre}">
+                <input type="text" name="talle" value="${producto.talle}">
+                <input type="number" name="precio" value="${producto.precio}">
+                <button class="btn" type="submit">Editar</button>
+            </form>
+            <button class="btn">Borrar</button>`
+    ContenedorProductos.append(tarjetaProducto)
+    })
+    
+}
+crearProducto()
+verProductos()
 /*
 const carrito = []
 let total = 0
@@ -58,11 +223,11 @@ const verProductos = () =>{
  total = carrito.reduce((total,producto,codigoPostal) => total+ producto.precio+codigoPostal,0)
  }
 
-
+*/
 
 
     
-
+/*
 const vender = (producto) =>{
     
     while(producto){
